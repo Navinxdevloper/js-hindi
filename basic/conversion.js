4e729a342e6eaa2fc0a293a9c8b5057c03e3b989
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-10 08:24:11 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,24 @@
+// let age = "21"
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+// let valueinnumber = Number(age)
+// console.log(typeof valueinnumber);
+
+//"21" => 21
+//"21abc" => Nan
+// true =>1
+// false =>0
+
+// let isSignin = 1
+// let booleeanisSignin = Boolean(isSignin)
+// console.log(typeof booleeanisSignin);
+
+// let number = 444
+// let stringNumber = String(number)
+// console.log(typeof stringNumber);
+
+// let name = "ram"
+// let numberName =Number(name)
+// console.log(typeof numberName);

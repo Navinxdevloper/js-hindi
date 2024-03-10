5acb21750a3a077c2d519ae9ce19f1fe6eb5afaf
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-10 08:00:54 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,16 @@
+"use strict";
+//alert("hello") we  are using node js not browser
+let name = "navin"
+let age = 21
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+//string =>""
+//booleean => true /false
+// null => standalone value type=object
+// undefined =>      type=undefined
+// symbol => unique
+
+
+//object

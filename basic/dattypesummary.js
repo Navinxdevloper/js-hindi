a9a76a3b48f48e6c75161ad4d2060f612ac6cc61
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-10 14:32:03 +0000

    datatype summary

---
+++ basic/dattypesummary.js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 type : string , number , boolean , null , 
+// undefined, bigint, symbol
+
+//let score = 100; //number datatype
+//let marks  = 100.50; //numberdatatype
+//let name = "navin"; //string datatype
+//let isLoggedin = true; //boolean datatype
+//let userEmail; //undefined
+//let password = null; // null is a object datype
+//let id = Symbol('101') //symbol (unique)
+//let Anotherid = Symbol('101')  //symbol (unique)
+
+//console.log(id==Anotherid)
+
+
+
+//let bigNumber = 3456677764512345678909876543322n //bigInt dattype just enter 'n' in last
+
+// reference type (non-primitive)
+
+// non primitive
+// array, objects, functions
+
+// basic :
+
+// let heros = ["batman","wonderwoman","superman","flash","aquaman"] //Array is a object datatype
+
+// let profile = {
+//     name:"navin",
+//     age: 21,
+//     course:"web devlopment"
+// } //objectdatatype
+
+// let myFunction = function(){
+// console.log("Hello world");
+// } // function it is a object function datatype
+
+//console.log(typeof score)

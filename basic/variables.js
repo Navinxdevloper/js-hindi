11024246a22b29e377a8cc74d319bbdcea6caa72
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-10 07:02:56 +0000

    variable

---
+++ basic/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144567;
+let accountEmail = "navinxvish@gmail.com";
+var accountPassword = "123";
+accountCity = "mumbai";
+let accountState;
+
+//accountId = 1 not allowed
+accountEmail="abc@gmail.com"
+accountPassword="2345"
+accountCity="delhi"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountEmail);
+console.log(accountCity);
+
+console.table({accountId,accountEmail,accountPassword,accountCity,accountState})

2c673dea3983c728173445d18f00084edcd60f72
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-10 09:13:05 +0000

    comparison

---
+++ basic/comparison.js
@@ -0,0 +1,36 @@
+//comparison in int
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 == 3);
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 != 3);
+
+//comparison in string
+// console.log("2" < 2);
+// console.log("2" > 2);
+// console.log("2" == 2);
+// console.log("2" >= 2);
+// console.log("2" <= 2);
+// // console.log("2" != 2);
+
+//comparison in str with boolean
+// console.log("2" > true);
+// console.log("2" < false);
+
+//comparison with null value
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(null == 0)
+// console.log(null != 0)
+
+//comparison with undefined
+// console.log(undefined > 0) 
+// console.log(undefined < 0) 
+// console.log(undefined >= 0) 
+// console.log(undefined <= 0) 
+// console.log(undefined == 0) 
+// console.log(undefined != 0) 
+

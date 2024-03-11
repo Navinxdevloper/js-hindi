801b44e09a9f67d0a538b8ea3d4642ed86cf920a
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-11 14:01:17 +0000

    dateandtime

---
+++ basic/dateandtime.js
@@ -0,0 +1,34 @@
+// const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// let date = new Date(2024,5,12)
+// console.log(date.toLocaleString());
+
+// let newdate = new Date(2024,3,5,11,23)
+// console.log(newdate.toLocaleString());
+
+ //let newdate = new Date("1-7-2024")
+//console.log(newdate.toLocaleString());
+
+//let timestamp = Date.now()
+
+// console.log(timestamp);
+//console.log(Math.floor(Date.now()/1000));
+
+
+// let newDate = new Date("1-3-2023")
+// console.log(newDate.getDate())
+// console.log(newDate.getMonth())
+// console.log(newDate.getHours())
+// console.log(newDate.getDate())
+// console.log(newDate.getDate())
+
+
+// newDate.toLocaleDateString('default', {
+//     weekday : "long",
+// })

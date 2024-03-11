5b5738a98e458026cc2c24709b4abc030df540b0
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-11 06:15:57 +0000

    math and number

---
+++ basic/numandmaths.js
@@ -0,0 +1,36 @@
+// const score = 400;
+
+// const  balance = new Number(25000)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const num = 144.36
+// console.log(num.toPrecision(4));
+
+// const number =  10000000000
+// console.log(number.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.4));
+// console.log(Math.round(4.4));
+// console.log((Math.random()*10)+1);
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 35
+const max = 60
+
+console.log(Math.floor(Math.random() *(max - min +1)+ min ))
--- basic/string.js
+++ basic/string.js
@@ -7,18 +7,20 @@
 //console.log(`hello my name is ${name} and my repocount is ${repocount}`);
 
 
-const myname = new String(`navin-sabka-baap-hai`)
+// const myname = new String(`navin-sabka-baap-hai`)
 
-console.log(myname.toLowerCase());
-console.log(myname.toUpperCase());
-console.log(myname.trim())
-console.log(myname.length);
-console.log(myname.slice(1,4));
-console.log(myname.charAt(`1`));
-console.log(myname.indexOf(`n`));
-console.log(myname.includes(`hai`));
-console.log(myname.endsWith(`hai`));
-console.log(myname.startsWith(`hai`));
+// console.log(myname.toLowerCase());
+// console.log(myname.toUpperCase());
+// console.log(myname.trim())
+// console.log(myname.length);
+// console.log(myname.slice(1,4));
+// console.log(myname.charAt(`1`));
+// console.log(myname.indexOf(`n`));
+// console.log(myname.includes(`hai`));
+// console.log(myname.endsWith(`hai`));
+// console.log(myname.startsWith(`hai`));
+
+
 
 
 

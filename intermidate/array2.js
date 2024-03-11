293e7f0be5c7d5d5cd3db807c9ccb401ecbc44a4
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-11 15:03:50 +0000

    array2

---
+++ intermidate/array2.js
@@ -0,0 +1,27 @@
+const DChero = ["batman","wonderwoman","superman","flash","greenlantern","hawkgirl"]
+const marvelHero = ["ironman","captainAmerica","thor","hulk"]
+
+// marvelHero.push(DChero)
+// console.log(marvelHero);
+// console.log(marvelHero[4][1]);
+
+// let heros = marvelHero.concat(DChero)
+// console.log(heros);
+
+// const allHeros = [...marvelHero, ...DChero]
+// console.log(allHeros);
+
+
+// const array = [1,2,3,[4,5,6,7],8,9,[2,1,[4,5]]]
+
+// const real = array.flat(Infinity)
+// console.log(real);
+
+// console.log(Array.isArray("navin")) // it is used to check the give value is array or not
+// console.log(Array.from("navin"))// it is used to converted given value into array
+// console.log(Array.from({name : "navin"})) //this way you can not creat array
+// const score = 100
+// const name = "navin"
+// const middlename = "rajnath"
+// const lastname =" vishwakarma"
+// console.log(Array.of(name,middlename,lastname)) // it is used to converted given value into array

80802ca680d094c9aa668edd843b29919f9ebaab
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-12 14:49:27 +0000

    IIFE

---
+++ intermidate/iifs.js
@@ -0,0 +1,17 @@
+//immadiately invoked function expression(iife)
+
+
+//named IIFE ex:
+(function doodh(){
+    console.log(`chai ban rahi hai`);
+})();
+
+
+//unnamed IIFE:
+((kadipatta) =>{
+    console.log(`chai me  ${kadipatta} ki kami hai`);
+})('tata hole')
+
+
+//last me ; ye lagana jaruri hai nhi toh do iife ek saath run nhi hoga
+

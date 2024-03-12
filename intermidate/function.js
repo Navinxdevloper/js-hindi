4d979d36097739c4c7b84fa8aa534a12a9ee1efb
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-12 06:11:36 +0000

    function

--- intermidate/function.js
+++ intermidate/function.js
@@ -29,16 +29,43 @@ function addNumber(a,b) {
 // console.log(loginUsermessage("navin"))
 
 
-function isLoggedin(username = "navin") {
-    if(!username){
-        console.log("Please enter valid username")
-        return
-    }
-    else{
-        return`${username} just logged in`
-    }
-}
+// function isLoggedin(username = "navin") {
+//     if(!username){
+//         console.log("Please enter valid username")
+//         return
+//     }
+//     else{
+//         return`${username} just logged in`
+//     }
+// }
 
 //console.log(isLoggedin("navin"));
-console.log(isLoggedin());
-console.log(isLoggedin("vinit"));
+//console.log(isLoggedin());
+//console.log(isLoggedin("vinit"));
+
+
+function morenumber(...num){
+    return num
+}
+
+//console.log(morenumber(200,400,500,600,700,800,900))
+
+// const obj ={
+//     username : "navin",
+//     salary : 55000,
+//     age : 21
+// }
+
+// function isloggedin(getuser){
+//     console.log(`my name is ${getuser.username} and my age is ${getuser.age} and my salary is ${getuser.salary} `)
+// }
+// console.log(isloggedin(obj));
+
+
+const newArray = [200,100,400,500,600]
+
+function oldArray(getArray){
+    return getArray[4]
+}
+
+console.log(oldArray(newArray))

c28d1d4889d9d7a2dbc0ff60f6046857007288a5
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-12 06:29:09 +0000

    bohot scope hai

---
+++ intermidate/scope.js
@@ -0,0 +1,12 @@
+// scope 
+
+// there are two typpes of scope
+
+// 1) block scope = {} age value ya loop  , function , ya conditional statment jab tak { } iske ander hai wo block scope hoga 
+// isme hum let var or const ka bhi use kar sakte hai par var hamara global scope hoga or ham agar let or const ko iske andar declare 
+// kar rahe hai toh hame console.log bhi iske { } andar karwana hoga par var ko hum kahi bhi print  karwa sakte hai isliye var ka use nhi
+// karna chahiye
+
+
+// 2) global scope = agar ham value ya koi bhi statment iske {} bahar declare kar rahe hai toh wo global scope mana jayega isme agar ham letor const bhi iske bahar declare kar rahe hai 
+// toh iske {} bahar bhi print karwa sakte hai

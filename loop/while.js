f5156803978df9214cb51ef88024cb0253cc221c
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-13 07:41:35 +0000

    while

---
+++ loop/while.js
@@ -0,0 +1,14 @@
+
+// let index = 1
+// while (index<=10) {
+//     console.log(index);
+//     index++
+// }
+
+let myArray = ["superman","batman","flash","wonderwoman"]
+let index = 0
+while (index<myArray.length) {
+    const Element = myArray[index]
+    console.log(Element)
+    index++
+}

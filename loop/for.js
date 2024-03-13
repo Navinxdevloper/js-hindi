ad74aec47975f3873e27c19c2ba919871a22de51
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-13 07:28:39 +0000

    for

---
+++ loop/for.js
@@ -0,0 +1,29 @@
+//for 
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is best number");
+//     }
+//   console.log(element);
+
+// }
+
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop : ${i}`);
+//     for (let j = 0; j <=10; j++) {
+//    console.log(`inner loop  ${j} and inner loop ${i}`);
+// }
+    
+// }
+
+// let myArray = ["superman","batman","flash","wonderwoman"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+//break : break me agar condition match ho gaya toh loop wahi par band ho jayega
+// continue : continue condition match hone par jis value par condition laga hoga use skip karke loop khatam karega

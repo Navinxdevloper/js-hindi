0f9622ebd6fe80dda9b89b1658b145147e234734
NAVINXDEVLOPER <160208173+Navinxdevloper@users.noreply.github.com>
2024-03-25 12:46:08 +0000

    array

--- intermidate/array2.js
+++ intermidate/array2.js
@@ -1,20 +1,20 @@
 const DChero = ["batman","wonderwoman","superman","flash","greenlantern","hawkgirl"]
 const marvelHero = ["ironman","captainAmerica","thor","hulk"]
 
-// marvelHero.push(DChero)
+// marvelHero.push(DChero) //ye sirf last index me push kardega dusre array ko
 // console.log(marvelHero);
 // console.log(marvelHero[4][1]);
 
-// let heros = marvelHero.concat(DChero)
+// let heros = marvelHero.concat(DChero) // ye 2 array ko jodne ka kaam karega
 // console.log(heros);
 
-// const allHeros = [...marvelHero, ...DChero]
+// const allHeros = [...marvelHero, ...DChero] //ye sabse easy way 2 array ko jodne ka or isko spread oprator kahte hai
 // console.log(allHeros);
 
 
 // const array = [1,2,3,[4,5,6,7],8,9,[2,1,[4,5]]]
 
-// const real = array.flat(Infinity)
+// const real = array.flat(Infinity)//ye sare nested array ko ek me karne ka kaam karega
 // console.log(real);
 
 // console.log(Array.isArray("navin")) // it is used to check the give value is array or not
